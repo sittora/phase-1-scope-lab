2e6822d944859c952f1ea6481d8ff326982e9937
sittora <sittora@gmail.com>
2021-08-19 17:24:01 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = 'bob';
+const leastFavoriteCustomer = 'karen';
+
+
+function upperCaseCustomerName(name) {
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(name) {
+   bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'sveta';
+}
